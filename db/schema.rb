b633b0a4b44c634d5422896d8c054007b04328ba
shunking-tech <shnktech@gmail.com>
2019-10-22 16:07:58 +0900

    contractsテーブルを作成
    直前にcontractモデルを作成してある
    bundle exec rake db:migrate
    を実行
    あとでカラムを追加する必要あり

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_22_065939) do
+ActiveRecord::Schema.define(version: 2019_10_22_070242) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,11 @@ ActiveRecord::Schema.define(version: 2019_10_22_065939) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "contracts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
