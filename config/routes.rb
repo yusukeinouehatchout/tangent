1b3f611a3346590767fbf75026c5e1c119e471c7
shunking-tech <shnktech@gmail.com>
2019-10-23 19:44:13 +0900

    contracts#showアクションのパスを変更
    パラメーターの"id"が”:id”で固定されていたため

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :contracts, only: [:new, :create, :index]
   get 'contracts_search' => 'contracts#search'
-  post 'contracts/:id' => 'contracts#show'
+  post 'contracts/show' => 'contracts#show'
 end
