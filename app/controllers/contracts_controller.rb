f1586bc5242d5ee6844cb433b61555dd4f3ebc7d
shunking-tech <shnktech@gmail.com>
2019-10-23 15:28:07 +0900

    アップロードファイルを保存する処理
    ストロングパラメーターの作成
    createアクションの作成

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -2,4 +2,15 @@ class ContractsController < ApplicationController
   def new
     @contract = Contract.new
   end
+
+  def create
+    @contract = Contract.new(upload_params)
+    @contract.save
+  end
+
+  private
+  
+  def upload_params
+    params.require(:contract).permit(:pdf_file)
+  end
 end
