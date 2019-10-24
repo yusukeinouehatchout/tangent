9f4a8fa5a8676d731f88144881f932a9f3e1e488
中村駿輝 <shnklife@gmail.com>
2019-10-24 12:52:09 +0900

    仮のrootパスを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :contracts, only: [:new, :create, :index]
-  get 'contracts_search' => 'contracts#search'
+  # get 'contracts_search' => 'contracts#search'
+  root 'contracts#search'
   post 'contracts/show' => 'contracts#show'
 end
