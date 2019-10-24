f7eb226ebb0f7c905818d7f6e6d8991166252a68
中村駿輝 <shnklife@gmail.com>
2019-10-24 13:22:13 +0900

    仮のrootパス設定、rubyのバージョン戻す

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :contracts, only: [:new, :create, :index]
-  # get 'contracts_search' => 'contracts#search'
-  root 'contracts#search'
+  # resources :contracts, only: [:new, :create, :index]
+  resources :contracts, only: [:create, :index]
+  get 'contracts_search' => 'contracts#search'
+  root 'contracts#new'
   post 'contracts/show' => 'contracts#show'
 end
