88869b87bae203e07bd76ee1aaa77e2dfb7878e4
中村駿輝 <shnklife@gmail.com>
2019-10-24 21:20:03 +0900

    contractsテーブルにusersテーブルの外部キーを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_24_111049) do
+ActiveRecord::Schema.define(version: 2019_10_24_121337) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 2019_10_24_111049) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "pass"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_contracts_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -53,4 +55,5 @@ ActiveRecord::Schema.define(version: 2019_10_24_111049) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "contracts", "users"
 end
