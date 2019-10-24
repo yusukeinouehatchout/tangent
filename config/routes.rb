28a5e7753695641f875c8b99a2e8a20ad94e556f
中村駿輝 <shnklife@gmail.com>
2019-10-24 20:40:02 +0900

    マイページの表示まで
    users#showアクション作成
    ビュー作成
    ルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: [:show]
   resources :contracts, only: [:new, :create, :index]
   root 'contracts#search'
   post 'contracts/show' => 'contracts#show'
