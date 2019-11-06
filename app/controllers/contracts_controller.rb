f635c9cc57d7f86c24ebc092c697d58a92f1d670
中村駿輝 <shnklife@gmail.com>
2019-11-06 21:03:55 +0900

    不要なコメントを削除

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -4,7 +4,6 @@ class ContractsController < ApplicationController
   end
 
   def create
-    # binding.pry
     @contract = Contract.new(upload_params)
     @contract.save
     redirect_to contracts_path
@@ -31,28 +30,14 @@ class ContractsController < ApplicationController
 
   def create_pdf
     sign = Sign.create!(image_data_uri: params[:sign])
-    # sign_url = "public/uploads/store/" + sign.image_data.match(/{\"id\":\"(.+)\",\"storage/)[1]
     sign_url = "public/" + sign.image.url
     contract_url = "public/" + find_contract.pdf.url
 
-    
     respond_to do |format|
       format.pdf do
         sign_pdf = RecordPdf.new(sign_url).render
 
-        # active storageからpdfのURLを取得しようとしたが、どれも上手く使えなかった
-        # contract_url = polymorphic_url(find_contract.pdf_file)
-        # contract_url = open(find_contract.pdf_url).path
-        # contract_url = StringIO.open(find_contract.pdf_file.download)
-        # binding.pry
-        
-        # contract_pdf = RecordPdf.new(contract_url).render
-        # contract_pdf = RecordPdf.new("contract.pdf", template: contract_url).render
-        # contract_pdf = PDF::Reader.new(contract_url)
-        # binding.pry
-
         combine_pdf = CombinePDF.new
-        # combine_pdf << CombinePDF.parse(contract_pdf)
         combine_pdf << CombinePDF.load(contract_url)
         combine_pdf << CombinePDF.parse(sign_pdf)
         combine_pdf.save "combined.pdf"
@@ -64,16 +49,6 @@ class ContractsController < ApplicationController
       end
     end
 
-    # 書名PDFを表示する処理を退避
-    # respond_to do |format|
-    #   format.pdf do
-    #     sign_pdf = RecordPdf.new(sign_url).render
-    #     send_data sign_pdf,
-    #       filename:    'test.pdf',
-    #       type:        'application/pdf',
-    #       disposition: 'inline' # 画面に表示
-    #   end
-    # end
     sign.destroy
   end
 
