78615367a4fa073887ee917317f3cbc972baf07b
中村駿輝 <shnklife@gmail.com>
2019-11-11 20:51:21 +0900

    アソシエーションの修正
    外部キーを別名で作る場合、アソシエーションでアソシエーション先のクラス名やカラム名を明示的に記述する必要がある

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,4 +1,4 @@
 class Contract < ApplicationRecord
   include PictureUploader::Attachment.new(:pdf)
-  has_many :signed_contracts, dependent: :destroy
+  has_many :signed_contracts, class_name: "SignedContract", foreign_key: "templete_id", dependent: :destroy
 end
