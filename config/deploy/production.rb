293c80ae01d064138c9d4b0a941b432cd5052d9b
中村駿輝 <shnklife@gmail.com>
2019-12-04 03:13:46 +0900

    ssh接続をrootにしてみた

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '133.167.41.77', user: 'deploy', roles: %w{app db web}, port: 10022 
+server '133.167.41.77', user: 'root', roles: %w{app db web}, port: 10022 
 
 #デプロイするサーバーにsshログインする鍵の情報。サーバー編で作成した鍵のパス
-set :ssh_options, keys: '~/.ssh/tangent.pem'
+# set :ssh_options, keys: '~/.ssh/tangent.pem'
