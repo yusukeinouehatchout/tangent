1e9970dff6671b6320f0014507ee5940cadc9cc3
中村駿輝 <shnklife@gmail.com>
2019-12-23 13:10:27 +0900

    トップページを設置(中身はまだ)
    トップページの画面を作成
    ルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # devise_for :users
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
     :sessions => 'users/sessions'   
@@ -13,7 +12,9 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
   resources :contracts, only: [:new, :create, :index, :destroy]
   resources :signed_contracts, only: [:create, :index, :show, :destroy]
-  root 'contracts#search'
+
+  root 'top#index'
+  get 'contracts/search' => 'contracts#search'
   post 'contracts/show' => 'contracts#show'
   post 'contracts/combine' => 'contracts#combine'
   post 'contracts/create_signed_pdf' => 'contracts#create_signed_pdf'
