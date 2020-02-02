30315954abeb5271a22bef701e4990cad2d6433f
中村駿輝 <shnklife@gmail.com>
2020-02-03 01:10:17 +0900

    Revert "js記述ファイルを変更"
    This reverts commit fdfd46ce4511af7b0b0f4d039a9f5a11c337c9cd.

--- app/assets/javascripts/documents_grid.js
+++ app/assets/javascripts/documents_grid.js
@@ -1,13 +1,13 @@
-// console.log('aaaaaaaaaa')
+console.log('aaaaaaaaaa')
 
-// $(function(){
-//   $('.document_id').click(function(){
-//     var document_id = $(this).text()
-//     var document_id_only_number = document_id.replace(/[^0-9]/g, '')
-//     console.log(document_id_only_number)
-//     $('.docuent_menu').html('<a class="contract-confirm" href="/signed_contracts">全ての提出された書類を確認する</a>' + '<br><br><br>' +
-//                             document_id + '<br><br>' +
-//                             `<a class="contract-confirm" href="/signed_contracts/every_templete_index?templete_id=${document_id_only_number}">提出された書類を確認する</a>` + '<br><br>' +
-//                             `<a data-confirm="この原本に提出された書類も削除されます。本当に削除しますか？" class="contract-delete" rel="nofollow" data-method="DELETE" href="/contracts/${document_id_only_number}">削除</a>`)
-//   })
-// });
+$(function(){
+  $('.document_id').click(function(){
+    var document_id = $(this).text()
+    var document_id_only_number = document_id.replace(/[^0-9]/g, '')
+    console.log(document_id_only_number)
+    $('.docuent_menu').html('<a class="contract-confirm" href="/signed_contracts">全ての提出された書類を確認する</a>' + '<br><br><br>' +
+                            document_id + '<br><br>' +
+                            `<a class="contract-confirm" href="/signed_contracts/every_templete_index?templete_id=${document_id_only_number}">提出された書類を確認する</a>` + '<br><br>' +
+                            `<a data-confirm="この原本に提出された書類も削除されます。本当に削除しますか？" class="contract-delete" rel="nofollow" data-method="DELETE" href="/contracts/${document_id_only_number}">削除</a>`)
+  })
+});
