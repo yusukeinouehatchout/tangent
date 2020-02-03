c3abfe59677e0f6130850890812550fe3d90821a
中村駿輝 <shnklife@gmail.com>
2020-02-03 15:01:58 +0900

    署名確認・削除ボタンを書類選択メニューにのみ表示

--- app/assets/javascripts/documents_grid.js
+++ app/assets/javascripts/documents_grid.js
@@ -4,9 +4,12 @@ $(function(){
   $('.document_id').click(function(){
     var document_id = $(this).text()
     var document_id_only_number = document_id.replace(/[^0-9]/g, '')
+    var sign_number = $(this).next('.sub-info').text()
+    console.log(sign_number)
     console.log(document_id_only_number)
     $('.docuent_menu').html('<a class="contract-confirm" href="/signed_contracts">全ての提出された書類を確認する</a>' + '<br><br><br>' +
-                            document_id + '<br><br>' +
+                            '<h3>' + document_id + '</h3>' + '<br><br>' +
+                            sign_number + '<br><br>' +
                             `<a class="contract-confirm" href="/signed_contracts/every_templete_index?templete_id=${document_id_only_number}">提出された書類を確認する</a>` + '<br><br>' +
                             `<a data-confirm="この原本に提出された書類も削除されます。本当に削除しますか？" class="contract-delete" rel="nofollow" data-method="DELETE" href="/contracts/${document_id_only_number}">削除</a>`)
   })
