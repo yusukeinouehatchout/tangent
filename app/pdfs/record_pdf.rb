931db448e008068b8adb711898c3011045c3ceb4
中村駿輝 <shnklife@gmail.com>
2020-04-04 16:56:36 +0900

    署名欄の作成

--- app/pdfs/record_pdf.rb
+++ app/pdfs/record_pdf.rb
@@ -1,18 +1,25 @@
 class RecordPdf < Prawn::Document
   def initialize(sign_url)
-    super()
+    super(page_size: 'A4')
 
-    image sign_url
-    # sign_url.render_body
+    # 現在の日付取得
+    t = Time.now
+    nowDate = "#{t.year}年#{t.month}月#{t.day}日"
+
+    # フォント指定して、日本語を使えるようにする
+    font 'app/assets/fonts/ipaexm.ttf'
+
+    text_box "#{nowDate}", size: 16, align: :right
+    text "署名", size: 24
+
+    # 署名画像を表示
+    image sign_url, height: 100
+
+    #下線を引く
+    line([0, 630], [500, 630])
+    line_width = 1
+    undash
+    stroke_color("000000")
+    stroke
   end
-
-  # def initialize(filename, template: "path")
-  #   # binding.pry
-
-  #   super()
-  # end
-
-  # def add_image(sign_url)
-  #   image sign_url
-  # end
 end
